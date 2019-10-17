d8932627619971b593e8340eb5522f5be46d63a7
Renan Vaqueli <renan.vaqueli@hotmail.com>
2019-10-17 14:45:07 -0300

    Changed permission verify to validate if the user has permission != 1

--- api/permission_verify.php
+++ api/permission_verify.php
@@ -1,6 +1,6 @@
 <?php
-if (isset($_SESSION['permission']) && $_SESSION['permission'] != 0)
-header('Location: ../../abeille/index.php');
+if (isset($_SESSION['permission']) && $_SESSION['permission'] != 1)
+    header('Location: ../../abeille/index.php');
 
 
 ?>
