c6ed43f40276aac62a831ece4984be9cbe3c83a6
Renan Vaqueli <renan.vaqueli@hotmail.com>
2019-11-04 22:04:35 -0300

    Fixed logo image on login page

--- login.php
+++ login.php
@@ -43,7 +43,9 @@ session_start();
           <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+              <div class="col-lg-6 d-none d-lg-block">
+                <div class="bg-login-image"></div>
+              </div>
               <div class="col-lg-6">
                 <div class="p-5">
                   <div class="text-center">
